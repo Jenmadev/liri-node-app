a3251d8ad49c9d4a75c8c2ae1bad3c68461bfdf7
Jenmadev <info@maknowsthat.com>
2017-08-22 14:35:41 -0700

    updated keys.js

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
-var twitterKeys = new Twitter({
+exports.twitterKeys = new Twitter({
 	consumerKey:process.env.TWITTER_CONSUMER_KEY,
 	consumerSecret:process.env.TWITTER_CONSUMER_SECRET,
 	tokenKey:process.env.TWITTER_ACCESS_TOKEN_KEY,
 	tokenSecret:process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-var spotifyKeys = new Spotify({
+exports.spotifyKeys = new Spotify({
  	id: process.env.SPOTIFY_ID,
  	secret: process.env.SPOTIFY_SECRET
 });
 
-var omdbKeys = new Omdb({
+exports.omdbKeys = new Omdb({
 	id: process.env.OMDB_API_KEY
-})
+});
